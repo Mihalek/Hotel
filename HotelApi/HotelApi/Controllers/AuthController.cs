3085d419401199199e720aa5c4fb02a4f80b774a
Michal Marciniak <michalmarekmarciniak@gmail.com>
2020-11-20 21:08:04 +0100

    Dodanie jwt token do AuthController

--- HotelApi/HotelApi/Controllers/AuthController.cs
+++ HotelApi/HotelApi/Controllers/AuthController.cs
@@ -2,9 +2,12 @@
 using HotelApi.IServices;
 using HotelApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelApi.Controllers
@@ -14,11 +17,46 @@ namespace HotelApi.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService authService;
-        public AuthController(IAuthService authService_)
+        private readonly IConfiguration config;
+        public AuthController(IAuthService authService_, IConfiguration config_)
         {
             authService = authService_;
+            config = config_;
+
         }
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
+        {
+            var userFromService = await authService.Login(userForLoginDTO.Username.ToLower(), userForLoginDTO.Password);
+
+            if (userFromService == null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromService.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromService.Username)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddHours(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
         {
